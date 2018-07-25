685146e2a96595d6438ef14ced21806f78f9d425
vklimov <vklimov@livemaster.ru>
2018-07-25 22:55:50 +0300

    added extractTextPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const conf = {
 	entry: './src/index.js',
@@ -19,13 +20,16 @@ const conf = {
 			},
 			{
 				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader'
+				})
 			},
 		]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin('styles.css'),
+	]
 };
 
 module.exports = (env, options) => {
